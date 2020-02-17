9eb3be777e8602730fbf8e88a6f68bfe3f6e4feb
Cjkeenan <cjkeenan@live.com>
2020-02-17 14:03:46 -0800

    Update main.c

--- flisr.X/main.c
+++ flisr.X/main.c
@@ -2,12 +2,14 @@
  * File:   main.c
  * Author: Colin Keenan, Mark Tippit
  *
- * Created on February 17, 2020, 12:15 PM
+ * Created on February 17, 2020, 1:37 PM
  */
 
-
 #include <xc.h>
+#include <avr/io.h>
 
-void main(void) {
-    return;
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
 }
